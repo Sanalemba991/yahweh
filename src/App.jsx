5952fcb19375b79c9c07a9d5010e009e52685a5f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-18 21:52:43 +0530

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Cart from "./cart";
+import Cart from "./rt";
 import Ram from "./Ram"
 
 function App() {
